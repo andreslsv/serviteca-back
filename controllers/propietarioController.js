3757850b7f0357db76f7bf5944cbef8cb70380d9
fasalasdms <fernando.salas@dms.ms>
2023-08-19 17:21:25 -0500

    Agregar crud completo básico de propietario

--- controllers/propietarioController.js
+++ controllers/propietarioController.js
@@ -10,4 +10,55 @@ getPropietarios = async (req, res, next) => {
     }
 }
 
-module.exports = { getPropietarios };
+getPropietario = async (req, res, next) => {
+    try {
+        const propietario = await Propietario.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.send(propietario);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+setPropietario = async (req, res, next) => {
+    try {
+        const propietario = await Propietario.update(
+            req.body,
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+        res.json(propietario);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+createPropietario = async (req, res, next) => {
+    try {
+        const propietario = await Propietario.create(req.body);
+        res.json(propietario);
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+deletePropietario = async (req, res, next) => {
+    try {
+        const propietario = await Propietario.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.json({eliminado:req.params.id});
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = { getPropietarios, getPropietario, setPropietario, createPropietario, deletePropietario };
--- routes/propietario.js
+++ routes/propietario.js
@@ -4,5 +4,9 @@ const propietarioController = require('../controllers/propietarioController');
 
 /* GET home page. */
 router.get('/', propietarioController.getPropietarios);
+router.get('/:id', propietarioController.getPropietario);
+router.post('/:id', propietarioController.setPropietario);
+router.post('/', propietarioController.createPropietario);
+router.delete('/:id', propietarioController.deletePropietario);
 
 module.exports = router;
