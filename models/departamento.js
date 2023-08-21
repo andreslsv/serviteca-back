a198292f6495b8b88fd0c2854a949eef4baa741f
fasalasdms <fernando.salas@dms.ms>
2023-08-21 12:24:44 -0500

    Agregar modelos de paises, departamentos y ciudades

---
+++ models/departamento.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Departamento extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            //Departamento.belongsToMany(models.Role, { through:'Role_Departamento', foreignKey:'permisoId'});
+        }
+    }
+    Departamento.init({
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        nombre: DataTypes.STRING,
+        departamentoId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        tableName: 'departamentos',
+        modelName: 'Departamento',
+    });
+    return Departamento;
+};
